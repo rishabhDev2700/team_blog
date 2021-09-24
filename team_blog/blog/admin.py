da776067ea30b639172241f36895ee785eb203ea
rishabhDev2700 <rishavkumar2700@gmail.com>
2021-09-25 01:28:01 +0530

    Added Author and BlogPost models and other settings.

--- team_blog/blog/admin.py
+++ team_blog/blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author, BlogPost
+
+admin.register(Author)
+admin.register(BlogPost)
--- team_blog/blog/models.py
+++ team_blog/blog/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_of_birth = models.DateField()
+
+
+class BlogPost(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    title = models.CharField(max_length=40)
+    content = models.TextField()
+    posted_on = models.DateTimeField(auto_now_add=True)
